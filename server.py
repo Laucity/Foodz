b259c5bf8dcc78fc8110c006ab303e7ba36bc4c6
Anish Balaji <balaji.anish8@gmail.com>
2017-06-04 23:48:42 -0700

    update ui and build some server flow

--- server.py
+++ server.py
@@ -3,17 +3,43 @@ from flask import Flask
 from key import *
 app = Flask(__name__)
 
+import json
+
 from yelpapi import YelpAPI
-yelp_api = YelpAPI(client_id, client_secret)
+yelp_api = YelpAPI(FUSION_ID, FUSION_KEY)
 
 
 my_lat = "37.868654"
 my_long = "-122.259153"
 
+test_query = {
+				'like': ['thai', 'japanese', 'chinese'],
+				'dislike': ['italian', 'french'],
+				'price': '2,3',
+				'open_now': 1,
+				'lat': my_lat,
+				'long': my_long,
+				'sort_by': 'rating'
+			 }
+
 @app.route("/")
 def hello():
-    search_results = yelp_api.search_query(latitude=my_lat, longitude=my_long, term="restaurants")
+    search_results = yelp_api.search_query(latitude=my_lat, longitude=my_long, term="restaurants", limit=1)
     return str(search_results)
+
+@app.route("/<q>")
+def query(q):
+	q = test_query #json.loads(q)
+	results = yelp_api.search_query(latitude=q['lat'], 
+									longitude=q['long'],
+									price=q['price'],
+									open_now=q['open_now'],
+									term='restaurants',
+									categories=",".join(q['like']),
+									limit=1
+									)
+	return str(results)
+
 
 if __name__ == "__main__":
     app.run()
--- webapp/js/init.js
+++ webapp/js/init.js
@@ -2,48 +2,96 @@
     $(function(){
 
         $('.button-collapse').sideNav();
-        $('.parallax').parallax();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+var lat;
+var long;
+
+function savePosition(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+}
+
 
 $(document).ready(function() {
 	
     Materialize.updateTextFields();
 
-});
+    navigator.geolocation.getCurrentPosition(savePosition);
 
-var auto_options = {
-            'Apple': null,
-            'Microsoft': null,
-            'Google': null
-            };
+    var query = {
+        'lat': lat,
+        'long': long,
+        'like': [],
+        'dislike': [],
+        'open_now': '',
+        'price': [],
+    };
 
-var cuisine_options = {
-    'Thai': null,
-    'Chinese': null,
-    'Japanese': null,
-    'Indian': null,
-    'Fusion': null,
-    'Burmese': null,
-    'Italian': null,
-    'French': null,
-};
+    var cuisine_options = {
+        'thai': null,
+        'chinese': null,
+        'japanese': null,
+        'indian': null,
+        'fusion': null,
+        'burmese': null,
+        'italian': null,
+        'french': null,
+    };
 
-$('.chips-wlist').material_chip({
-    autocompleteOptions: {
-        placeholder: 'Enter a food/cuising you like',
-        data: auto_options,
-        limit: Infinity,
-        minLength: 1
-    }
-});
+    $('.chips-wlist').material_chip({
+        autocompleteOptions: {
+            placeholder: 'Enter a food/cuisine you like',
+            data: cuisine_options,
+            limit: 3,
+            minLength: 1
+        }
+    });
 
-$('.chips-blist').material_chip({
-    autocompleteOptions: {
-        placeholder: 'Enter a food/cuising you aren\'t feeling',
-        data: auto_options,
-        limit: Infinity,
-        minLength: 1
-    }
+    $('.chips-blist').material_chip({
+        autocompleteOptions: {
+            placeholder: 'Enter a food/cuisine you aren\'t feeling',
+            data: cuisine_options,
+            limit: Infinity,
+            minLength: 1
+        }
+    });
+
+    $('.chips-wlist').on('chip.add', function(e, chip){
+        console.log(chip);
+        query.like.push(chip.tag);
+        console.log(query);
+    });
+
+    $('.chips-wlist').on('chip.delete', function(e, chip){
+        query.like.splice(query.like.indexOf(chip.tag), 1);
+    });
+
+    $('.chips-blist').on('chip.add', function(e, chip){
+        query.dislike.push(chip.tag);
+    });
+
+    $('.chips-blist').on('chip.delete', function(e, chip){
+        query.dislike.splice(query.dislike.indexOf(chip.tag), 1);
+    });
+
+    $('#price').change(function(e) {
+        if (e.originalEvent.target.checked) {
+            query.price.push(e.originalEvent.target.id);
+        } else {
+            query.price.splice(query.price.indexOf(e.originalEvent.target.id), 1);
+        }
+        console.log(e.originalEvent.target.checked);
+        console.log(query);
+    });
+
+    $('#preferences').change(function () {
+        // send query
+        // update stuff
+    });
+
+
 });
