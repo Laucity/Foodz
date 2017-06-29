72f9a615a7dcaafe792d37263f811b75d7d34e34
Anish Balaji <balaji.anish8@gmail.com>
2017-06-28 22:09:00 -0700

    LFA is performed in each query to sort yelp results wooo

--- server.py
+++ server.py
@@ -17,19 +17,32 @@ yelp_api = YelpAPI(FUSION_ID, FUSION_KEY)
 R_CATEGORIES = json.loads(open("r_categories.json", "r").read())
 CATEGORY_ALIAS_ENCODING = {}
 for i, R_CAT in enumerate(R_CATEGORIES):
-	CATEGORY_ALIAS_ENCODING[R_CAT['alias']] = i
+    CATEGORY_ALIAS_ENCODING[R_CAT['alias']] = i
 
 # LOAD IN PREFERENCES MAT
 PREF_FILENAME = "preferences.mat"
 def load_preference_mat():
-	# Returns none if no file exists
-	try:
-		PREF_FILE = sio.loadmat(PREF_FILENAME)
-	except:
-		PREF_FILE = None
-		print("No mat file exists")
+    # Returns none if no file exists
+    try:
+        PREF_FILE = sio.loadmat(PREF_FILENAME)
+    except:
+        PREF_FILE = None
+        print("No mat file exists")
 
-	return PREF_FILE
+    return PREF_FILE
+
+# reconstruct with SVD
+def reconstruct(matrix, rank=None):
+    U, s, V = np.linalg.svd(matrix, full_matrices=False)
+
+    cp = np.copy(s)
+
+    if rank is not None:
+        cp[rank:] = 0
+
+    S = np.diag(cp)
+    
+    return U @ (S @ V)
 
 # Thread Class for async requests
 class bThread (threading.Thread):
@@ -49,36 +62,35 @@ class bThread (threading.Thread):
 # FEAURE VECTOR CREATION
 def encode_business(business):
 
-	# Turn a business into a feature vector
-	
-	f_vector = np.zeros(len(R_CATEGORIES))
+    # Turn a business into a feature vector
+    f_vector = np.zeros(len(R_CATEGORIES))
 
-	categories = business['categories']
+    categories = business['categories']
 
-	indices = []
-	for c in categories:
-		try:
-			i = CATEGORY_ALIAS_ENCODING[c['alias']]
-			indices.append(i)
-		except:
-			print("CATEGORY NOT FOUND")
-			print(c['alias'])
+    indices = []
+    for c in categories:
+        try:
+            i = CATEGORY_ALIAS_ENCODING[c['alias']]
+            indices.append(i)
+        except:
+            print("CATEGORY NOT FOUND")
+            print(c['alias'])
 
-	# one hot encoding
-	f_vector[indices] = 1
+    # one hot encoding
+    f_vector[indices] = 1
 
-	return f_vector
+    return f_vector
 
 # Query format
 # test_query = {
-# 				'like': ['thai', 'japanese', 'chinese'],
-# 				'dislike': ['italian', 'french'],
-# 				'price': '2,3',
-# 				'open_now': 1,
-# 				'lat': "37.868654",
-# 				'long': "-122.259153",
-# 				'sort_by': 'rating'
-# 			 }
+#               'like': ['thai', 'japanese', 'chinese'],
+#               'dislike': ['italian', 'french'],
+#               'price': '2,3',
+#               'open_now': 1,
+#               'lat': "37.868654",
+#               'long': "-122.259153",
+#               'sort_by': 'rating'
+#            }
 
 @app.route("/")
 def hello():
@@ -88,85 +100,118 @@ def hello():
 @app.route("/<q>", methods=['GET'])
 def query(q):
 
-	query = json.loads(request.args.get('query'))
-	print("QUERY")
-	print(query)
+    query = json.loads(request.args.get('query'))
+    print("QUERY")
+    print(query)
 
-	q = query #json.loads(q)
-	results = yelp_api.search_query(latitude=q['lat'], 
-									longitude=q['long'],
-									price=",".join(q['price']),
-									open_now=q['open_now'],
-									term='restaurants',
-									categories=",".join(q['like']),
-									limit=10
-									)
-	# pull extra business data
-	businesses = results['businesses']
+    q = query #json.loads(q)
+    results = yelp_api.search_query(latitude=q['lat'], 
+                                    longitude=q['long'],
+                                    price=",".join(q['price']),
+                                    open_now=q['open_now'],
+                                    term='restaurants',
+                                    categories=",".join(q['like']),
+                                    limit=10
+                                    )
+    # pull extra business data
+    result_businesses = results['businesses']
 
-	# OLD
-	# for business in businesses:
-	# 	b_id = business['id']
-	# 	extra_info = yelp_api.business_query(id=b_id)
-	# 	business['extra_details'] = extra_info
+    # OLD
+    # for business in businesses:
+    #   b_id = business['id']
+    #   extra_info = yelp_api.business_query(id=b_id)
+    #   business['extra_details'] = extra_info
 
-	# WITH LFA
-	# PUll 50 businesses from the query
-	# check if they have been encoded
-	# encode those that have not been
-	# rescore all the restaurants
-	# sort them
+    # TODO
+    ### Take into account previously rated restaurants
+    ### add more features
+    ### account for current query
 
-	threads = []
-	for i, business in enumerate(businesses):
-		new_t = bThread(i, business)
-		new_t.start()
-		threads.append(new_t)
+    # LFA
+    PREF_FILE = load_preference_mat()
 
-	for t in threads:
-		t.join()
+    if PREF_FILE is not None:
+        businesses, scores = PREF_FILE['businesses'], PREF_FILE['scores']
 
-	print("All threads complete")
+        # add resulting businesses
+        for business in result_businesses:
+            encoded = encode_business(business)
 
-	response = jsonify(results)
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	return response
+            businesses = np.append(businesses, [encoded], axis=0)
+            scores = np.append(scores, 0)
+
+        user_matrix = np.c_[businesses, scores]
+        LFA_matrix = reconstruct(user_matrix, None) # rank = None for TESTING
+
+        results_start_index = len(businesses) - len(result_businesses)
+
+        lfa_scores = LFA_matrix[:,-1]
+        result_scores = lfa_scores[results_start_index:]
+
+        assert(len(result_scores) == len(result_businesses))
+
+        score_index_zip = zip(result_scores, list(range(len(result_scores))))
+        sorted_scores = list(score_index_zip)
+        sorted_scores.sort(key=lambda x: -x[1])
+
+        sorted_results = []
+        for score, index in sorted_scores:
+            sorted_results.append(result_businesses[index])
+
+    else:
+        sorted_results = results
+
+    # GET ADDITIONAL BUSINESS INFORMATION
+    threads = []
+    for i, business in enumerate(sorted_results):
+        new_t = bThread(i, business)
+        new_t.start()
+        threads.append(new_t)
+
+    for t in threads:
+        t.join()
+
+    print("All threads complete")
+
+    response = jsonify(results) # CHANGE
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route("/pref/<p>", methods=['GET'])
 def pref(p):
-	# Handle preference sent to server
+    # Handle preference sent to server
 
-	business = json.loads(request.args.get('business'))
-	score = json.loads(request.args.get('score'))
+    business = json.loads(request.args.get('business'))
+    score = json.loads(request.args.get('score'))
 
-	# Add to the preference matrix
-	encoded_business = encode_business(business)
+    # Add to the preference matrix
+    encoded_business = encode_business(business)
 
-	pref_file = load_preference_mat()
+    pref_file = load_preference_mat()
 
-	if pref_file is not None:
-		businesses = pref_file["businesses"]
-		scores = pref_file["scores"]
+    if pref_file is not None:
+        businesses = pref_file["businesses"]
+        scores = pref_file["scores"]
 
-		businesses = np.append(businesses, [encoded_business], axis=0)
-		scores = np.append(scores, score)
+        businesses = np.append(businesses, [encoded_business], axis=0)
+        scores = np.append(scores, score)
 
-	else:
-		businesses = np.array([encoded_business])
-		scores = np.array([score])
+    else:
+        businesses = np.array([encoded_business])
+        scores = np.array([score])
 
-	# save mat with new preference
-	assert(len(businesses) == len(scores))
-	mat_dict = {
-				"businesses": businesses, 
-				"scores": scores
-				}
-	sio.savemat(PREF_FILENAME, mat_dict)
+    # save mat with new preference
+    assert(len(businesses) == len(scores))
+    mat_dict = {
+                "businesses": businesses, 
+                "scores": scores
+                }
+    sio.savemat(PREF_FILENAME, mat_dict)
 
-	# Acknowledge preference to client
-	response = jsonify(["preference noted"])
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	return response
+    # Acknowledge preference to client
+    response = jsonify(["preference noted"])
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
     app.run()
